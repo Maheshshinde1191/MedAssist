aedcec6f42e5489ea921123a0234561180d3efcc
Maheshshinde1191 <maheshshinde1191@gmail.com>
2024-05-17 20:43:07 +0530

    Code pushed

---
+++ app/src/main/java/com/example/sickpredict/utils/DoctorData.kt
@@ -0,0 +1,41 @@
+package com.example.sickpredict.utils
+
+import com.example.sickpredict.data.doctor.Doctor
+
+object DoctorData {
+    val doctorList = arrayListOf(
+        Doctor(
+            "Dr. Nikita Dhumal(Ranawade)",
+            "General Family Physician, B.H.M.S.",
+            "1 year",
+            "5",
+            "300",
+            arrayListOf("Cardiologist", "Dentist"),
+            "",
+            "2WMhBTyFxgeJOU6YET50ZnGOq9j2"
+        ),
+        Doctor(
+            "Dr. Suyog Kalbhor",
+            "MBBS, MD",
+            "5 years",
+            "4.5",
+            "500",
+            arrayListOf("Child Specialist", "Skin Specialist", "Dermatologist"),
+            "",
+            "6SKFZ3r8NYOmaYUqlEXFHJm2fZp1"
+        ),
+        Doctor(
+            "Dr. Kiran Kalbhor",
+            "MBBS, MD",
+            "3 years",
+            "4.5",
+            "500",
+            arrayListOf("Cardiologist", "Dentist"),
+            "",
+            "sZkJJFsmlldSjr2iawsu38IJT612"
+        ),
+    )
+
+
+
+}
